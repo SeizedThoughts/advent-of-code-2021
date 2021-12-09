664a3db2c10dc791ebd7de19caf45a84e575f761
SeizedThoughts <seizedthoughts@gmail.com>
2021-12-08 20:50:55 -0800

    update template

--- day-n/index.py
+++ day-n/index.py
@@ -1,4 +1,4 @@
-input = [(i) for i in open('day-7/input.txt', 'r').read().split('\n')]
+input = [(i) for i in open('day-n/input.txt', 'r').read().split('\n')]
 
 for i in range(len(input)):
     line = input[i]
